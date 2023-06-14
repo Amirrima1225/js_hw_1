a4f4d6be9812bcb2afc54744c5f3a29bc1eb40e6
Амиррима1225 <amirsharofov1@gmail.com>
2023-06-14 12:31:16 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let products = [
+    {
+        name: "apple",
+        price:12000
+    },
+    {
+        name: "cucumber",
+        price:12000
+    },
+    {
+        name: "apple",
+        price:12000
+    },
+    {
+        name: "apple",
+        price:12000
+    },
+    {
+        name: "apple",
+        price:12000
+    },
+    {
+        name: "apple",
+        price:12000
+    },
+]
+
+let total = 0
+
+products.forEach(product => {
+    let salePrice = 30 * product.price - (30 * product.price / 100)
+    
+})
+
